8a4f19c2e35f9af50368844544a5dcbfa775c98d
Dohd <alloicebrian@gmail.com>
2025-10-21 16:15:19 +0300

    Fix: hide tid

--- app/Repositories/Focus/customer/CustomerRepository.php
+++ app/Repositories/Focus/customer/CustomerRepository.php
@@ -65,7 +65,7 @@ class CustomerRepository extends BaseRepository
         $q = $this->query();
         $q->orderByRaw("CASE WHEN LOWER(name) = LOWER(?) THEN 1 ELSE 2 END", ['Walk-In'])->orderByRaw('LOWER(name) ASC');
 
-        return $q->get(['id', 'tid', 'name','company','email', 'phone', 'currency_id', 'address','picture','active','created_at']);
+        return $q->get(['id', 'name','company','email', 'phone', 'currency_id', 'address','picture','active','created_at']);
     }
 
     /**
